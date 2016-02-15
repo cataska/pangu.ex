c0d779afe6f6fb109e9f03cff0a5d40d85a4a61d
Wen-Chun Lin <lovecankill@gmail.com>
2016-02-15 12:38:46 +0800

    Fix quote regex pattern

--- lib/pangu.ex
+++ lib/pangu.ex
@@ -12,11 +12,11 @@ defmodule Pangu do
   # https://github.com/vinta/pangu
 
   defp cjk_quote(str) do
-    Regex.replace ~r/([\x{2e80}-\x{2eff}\x{2f00}-\x{2fdf}\x{3040}-\x{309f}\x{30a0}-\x{30ff}\x{3100}-\x{312f}\x{3200}-\x{32ff}\x{3400}-\x{4dbf}\x{4e00}-\x{9fff}\x{f900}-\x{faff}])(["])/u, str, "\\1 \\2"
+    Regex.replace ~r/([\x{2e80}-\x{2eff}\x{2f00}-\x{2fdf}\x{3040}-\x{309f}\x{30a0}-\x{30ff}\x{3100}-\x{312f}\x{3200}-\x{32ff}\x{3400}-\x{4dbf}\x{4e00}-\x{9fff}\x{f900}-\x{faff}])(["'])/u, str, "\\1 \\2"
   end
 
   defp quote_cjk(str) do
-    Regex.replace ~r/(["])([\x{2e80}-\x{2eff}\x{2f00}-\x{2fdf}\x{3040}-\x{309f}\x{30a0}-\x{30ff}\x{3100}-\x{312f}\x{3200}-\x{32ff}\x{3400}-\x{4dbf}\x{4e00}-\x{9fff}\x{f900}-\x{faff}])/u, str, "\\1 \\2"
+    Regex.replace ~r/(["'])([\x{2e80}-\x{2eff}\x{2f00}-\x{2fdf}\x{3040}-\x{309f}\x{30a0}-\x{30ff}\x{3100}-\x{312f}\x{3200}-\x{32ff}\x{3400}-\x{4dbf}\x{4e00}-\x{9fff}\x{f900}-\x{faff}])/u, str, "\\1 \\2"
   end
 
   defp fix_quote(str) do
